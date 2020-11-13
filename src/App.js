bd1612bc2bf3515be3aa1c63ad045df9e15fa4de
Dorian Musaj <dorianmusajgithub@gmail.com>
2020-11-13 15:53:19 +0100

    add search bard and restaurant list in home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Input, Typography } from "antd";
+import "antd/dist/antd.css";
+
+import "./App.css";
+import SampleData from "./SampleData.json";
+import Restaurant from "./components/Restaurant/Restaurant";
+import { getFitleredRestaurants } from "./common/utils";
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+  const [product, setProduct] = useState("");
+  const [location, setLocation] = useState("");
+
+  const { Search } = Input;
+  const { Title } = Typography;
+
+  const jsonData = JSON.parse(JSON.stringify(SampleData));
+
+  const onSearch = (value) => {
+    if (value) {
+      const {
+        productSearched,
+        locationSearched,
+        filteredRestaurants,
+      } = getFitleredRestaurants(value, jsonData);
+
+      const sortedRestaurants = filteredRestaurants.sort((a, b) =>
+        b.Rank < a.Rank ? 1 : -1
+      );
+
+      setRestaurants(sortedRestaurants);
+      setProduct(productSearched);
+      setLocation(locationSearched);
+
+      console.log("Filtered restaurants:", sortedRestaurants);
+    } else {
+      setRestaurants([]);
+      setProduct("");
+      setLocation("");
+    }
+  };
+
+  const productSubTitle = product ? `${product} restaurants` : "";
+  const citySubTitle = location ? ` in ${location}` : "";
+  const searchTitle = restaurants.length
+    ? productSubTitle + citySubTitle
+    : "No result.";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ flex: "auto" }}>
+        <Search
+          placeholder="Search product or restaurant"
+          onSearch={onSearch}
+          enterButton
+          style={{ width: "300px" }}
+          size="medium"
+          allowClear
+        />
+      </div>
+      <div style={{ margin: "40px" }}>
+        {product && <Title level={5}>{searchTitle}</Title>}
+      </div>
+
+      <div style={{ flex: "auto" }}>
+        {restaurants.map((restaurant, i) => (
+          <Restaurant restaurant={restaurant} key={i} />
+        ))}
+      </div>
     </div>
   );
 }
